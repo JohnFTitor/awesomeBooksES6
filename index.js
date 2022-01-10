6d8ef337d35882d25f01d101ca78aaf42503fcf7
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-01-10 14:24:43 -0500

    Add DateTime using luxon installed with npm

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-import Bookshelf from "./modules/Bookshelf.js";
+import Bookshelf from './modules/Bookshelf.js';
+import { DateTime } from "/node_modules/luxon/src/luxon.js";
 
 const bookShelf = new Bookshelf();
 const addButton = document.getElementById('add-book-button');
 addButton.addEventListener('click', () => { bookShelf.addBook(); });
 bookShelf.displayBooks();
 
-
-
-//Navigation
+// Navigation
 
 function startTime() {
-    document.getElementById('date').innerHTML = new Date();
-    setTimeout(startTime, 1000);
+  const now = DateTime.now();
+  document.getElementById('date').innerHTML = now.toLocaleString(DateTime.DATETIME_MED);
+  setTimeout(startTime, 1000);
 }
 
 function toggleDisplay(id, link) {
-    const element = document.querySelectorAll('.content');
-    element.forEach((section) => section.classList.add('hide'));
-    const current = document.getElementById(id);
-    current.classList.remove('hide');
+  const element = document.querySelectorAll('.content');
+  element.forEach((section) => section.classList.add('hide'));
+  const current = document.getElementById(id);
+  current.classList.remove('hide');
 
-    const menuLinks = document.querySelectorAll('.menu-links');
-    menuLinks.forEach((selector) => selector.classList.remove('active'));
-    link.classList.add('active');
+  const menuLinks = document.querySelectorAll('.menu-links');
+  menuLinks.forEach((selector) => selector.classList.remove('active'));
+  link.classList.add('active');
 }
 
 const selectList = document.getElementById('list');
