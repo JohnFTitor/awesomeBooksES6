cbfbc376d5e466d7874a1c5f6a5d84231069609a
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-10 11:39:34 -0600

    Organize classes as modules and import to index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+import Bookshelf from "./modules/Bookshelf.js";
+
+const bookShelf = new Bookshelf();
+const addButton = document.getElementById('add-book-button');
+addButton.addEventListener('click', () => { bookShelf.addBook(); });
+bookShelf.displayBooks();
+
+
+
+//Navigation
+
+function startTime() {
+    document.getElementById('date').innerHTML = new Date();
+    setTimeout(startTime, 1000);
+}
+
+function toggleDisplay(id, link) {
+    const element = document.querySelectorAll('.content');
+    element.forEach((section) => section.classList.add('hide'));
+    const current = document.getElementById(id);
+    current.classList.remove('hide');
+
+    const menuLinks = document.querySelectorAll('.menu-links');
+    menuLinks.forEach((selector) => selector.classList.remove('active'));
+    link.classList.add('active');
+}
+
+const selectList = document.getElementById('list');
+selectList.addEventListener('click', () => { toggleDisplay('awesome-list', selectList); });
+
+const selectNew = document.getElementById('new');
+selectNew.addEventListener('click', () => { toggleDisplay('add-book', selectNew); });
+
+const selectContact = document.getElementById('contact');
+selectContact.addEventListener('click', () => { toggleDisplay('contact-info', selectContact); });
+
+startTime();
+toggleDisplay('awesome-list', selectList);
---
+++ modules/Bookshelf.js
@@ -0,0 +1,45 @@
+import Book from "./Books.js";
+
+export default class Bookshelf {
+    constructor() {
+        this.books = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
+        this.bookList = document.getElementById('book-list');
+    }
+
+    removeBook(index) {
+        this.books = this.books.filter((book) => this.books.indexOf(book) !== index);
+        this.storeData();
+    }
+
+    displayBooks() {
+        this.bookList.innerHTML = '';
+
+        this.books.forEach((book) => {
+            const rowHTML = (book) => `
+            <span class="book-title">"${book.title}" by ${book.author}</span>
+            <button class="remove-button" type="button">Remove</button>
+            `;
+            const rowTr = document.createElement('tr');
+            rowTr.innerHTML = rowHTML(book);
+            rowTr.querySelector('.remove-button').addEventListener('click', () => {
+                this.removeBook(this.books.indexOf(book));
+                this.displayBooks(this.books);
+            });
+            document.getElementById('book-list').appendChild(rowTr);
+        });
+    }
+
+    storeData() {
+        localStorage.setItem('books', JSON.stringify(this.books));
+    }
+
+    addBook() {
+        const book = new Book();
+        if (JSON.stringify(this.books[this.books.length - 1]) !== JSON.stringify(book)
+            && book.title !== '' && book.author !== '') {
+            this.books.push(book);
+            this.storeData();
+            this.displayBooks(this.books);
+        }
+    }
+}
